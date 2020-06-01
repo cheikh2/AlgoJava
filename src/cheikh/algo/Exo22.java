e59c9dd217992be04de56e11089ecf39fbff279c
Cheikh Mbow <cheikh2mbow@gmail.com>
2020-06-01 15:42:30 +0000

    1juin-commit

--- src/cheikh/algo/Exo22.java
+++ src/cheikh/algo/Exo22.java
@@ -6,41 +6,47 @@ public class Exo22 {
 
 	public static void main(String[] args) {
 		try (Scanner scan = new Scanner(System.in)) {
-		int N,i,pos,sequencours,maxsequence;
-		
-		int [ ]  tableau = new int[100];
-		
-		System.out.println("Entrer N entiers différent compris entre 10 et 50");
-		N=scan.nextInt();
-		
-		do {
-			System.out.println("Entrer N entiers différent compris entre 10 et 50");
-			N=scan.nextInt();
-			
-		}while(N <= 10 && N >= 500);
-		for(i=1; i<=N; i++) {
-			System.out.println("Entrer une valeur");
-			N=scan.nextInt();
+			 int N;
+		        do {
+		            System.out.println("Veuillez saisir la taille de la séquence : (entre 10 et 50)");
+		            N = scan.nextInt();
+		        } while (N < 10 || N > 50);
+		        int[] tab = new int[N];
+		        for (int i = 0; i < N; i++) {
+		            do {
+		            System.out.println("Veuillez saisir un nombre : ");
+		            int nombre = scan.nextInt();
+		            tab[i] = nombre;
+		            } while (N < 1 || N >= 100);
+		        }
+		        //  1  2  3  1  2  3  4  5  6  7  8  2  3  4  5 
+		        int longueur = 1;
+		        int position = 0;
+		        int longueur_i = 1;
+		        for (int i = 1; i < tab.length; i++) {
+		            if (tab[i] > tab[i - 1]) {
+		                longueur++;
+		            } else {
+		                if (longueur > longueur_i) {
+		                    longueur_i = longueur;
+		                    position = i - longueur_i;
+		                }
+		                longueur = 1;
+		            } 
+		            if(i==(tab.length-1)){
+		               if (longueur > longueur_i) {
+		                    longueur_i = longueur;
+		                    position = (i - longueur_i)+1;
+		                } 
+		            }
+		        }
+		        System.out.print("La plus longue séquence est ");
+		        for (int i = position; i < (longueur_i + position); i++) {
+		            System.out.print(tab[i] + " * ");
+		        }
+		        System.out.println("qui débute à la position " + (position + 1));
+		        System.out.println(" et elle est de longueur " + longueur_i);
+		    }
 		}
-				pos = 0;
-				sequencours = 1;
-				maxsequence = 0;
-				for(i=1; i<=N; i++) {
-					if((tableau[i] < tableau[i+1])){
-						sequencours = sequencours + 1;
-						if(sequencours > maxsequence ) {
-							  maxsequence = sequencours;
-							  pos = (i + 1)-maxsequence+1;
-						}
-						else {}
-						 sequencours = 1;
-
-					}
-				}
-				
-		}
-		
-
-	}
 
 }
